2243cee31983addfbe32784bc3d7cd2a2c622370
Boggs <evan.boggs@mantech.com>
2020-02-19 11:46:22 -0500

    sandbox has been created.... let the fun begin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import Dragon from "./Dragon";
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
 import "./App.css";
 
 function App() {
@@ -8,7 +10,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Dragon />
+        <DndProvider backend={Backend}>
+          <Dragon />
+        </DndProvider>
       </header>
     </div>
   );
