c4f6bd8b69b264ed8525e04458710d36219cdf16
Boggs <evan.boggs@mantech.com>
2020-02-19 07:43:13 -0500

    switching drag and drop libraries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import Dragon from "./Dragon";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Dragon />
       </header>
     </div>
   );
